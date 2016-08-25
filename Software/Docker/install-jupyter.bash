39def14f88bb6b8d41fdcde541f93d852e945259
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-08-25 16:54:49 -0700

    Jupyter scripts

---
+++ Software/Docker/install-jupyter.bash
@@ -0,0 +1,27 @@
+#! /bin/bash
+#
+# Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
+# License: AGPL-3
+
+# Jupyter itself
+source ${VIRTUALENVWRAPPER_SCRIPT}
+rmvirtualenv jupyter
+mkvirtualenv -p /usr/bin/python3 jupyter
+workon jupyter
+
+pip install --upgrade pip
+pip install jupyter
+pip install ipyparallel
+pip install virtualenvwrapper
+
+# R kernel
+R -e "IRkernel::installspec()"
+
+# create IPyParallel profile
+mkdir -p ~/.ipython/profile_default
+
+# extensions
+# see https://github.com/ipython/ipyparallel/issues/170
+jupyter serverextension enable --py ipyparallel --user
+jupyter nbextension install --py ipyparallel --user 
+jupyter nbextension enable --py ipyparallel --user
---
+++ Software/Docker/start-jupyter.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
+# License: AGPL-3
+
+# get rid of left-over PID files if any
+rm -f ~/.ipython/profile_default/pid/*.pid
+
+source $VIRTUALENVWRAPPER_SCRIPT
+workon jupyter
+jupyter notebook --port=8888 --ip=0.0.0.0 --no-browser
