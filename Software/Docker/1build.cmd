34c952ac5205a929b2d4d92222987b8b8eb2a4e3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-08-26 16:19:09 -0700

    no multi-line - confuses PowerShell

--- Software/Docker/1build.cmd
+++ Software/Docker/1build.cmd
@@ -1,5 +1,2 @@
-docker build \
-  --tag=znmeb/mastering-dfs-analytics:latest \
-  --file=Dockerfile \
-  .
+docker build --tag=znmeb/mastering-dfs-analytics:latest --file=Dockerfile .
 docker images
--- Software/Docker/2set-password.cmd
+++ Software/Docker/2set-password.cmd
@@ -1,10 +1,4 @@
-docker run \
-  --interactive \
-  --tty \
-  --user=root \
-  --name=dfstools \
-  znmeb/mastering-dfs-analytics:latest \
-  passwd dfstools
+docker run --interactive --tty --user=root --name=dfstools znmeb/mastering-dfs-analytics:latest passwd dfstools
 docker commit dfstools znmeb/mastering-dfs-analytics:latest
 docker rm dfstools
 docker images
