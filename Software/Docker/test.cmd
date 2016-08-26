5b180ed9fc2aa95335aba68762a9a1fef0893ae2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-08-25 18:39:36 -0700

    Jupyter port

--- Software/Docker/test.cmd
+++ Software/Docker/test.cmd
@@ -3,7 +3,7 @@ docker run \
   --tty \
   --user=root \
   --name=dfstools \
-  --publish=7878:7878 \
+  --publish=7777:7777 \
   docker.io/znmeb/mastering-dfs-analytics:latest \
   /usr/local/src/start-rstudio-server.bash
 docker commit dfstools docker.io/znmeb/mastering-dfs-analytics:latest
