34f6fd0a14a2e5eb99ca2830bdace2c75af70519
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-09-01 23:44:43 -0700

    no test load for rgl

--- Software/Docker/baseball.R
+++ Software/Docker/baseball.R
@@ -8,4 +8,5 @@ install.packages(c(
 devtools::install_github("cboettig/Sxslt", build_vignettes = TRUE)
 devtools::install_github("beanumber/openWAR", build_vignettes = FALSE)
 devtools::install_github("beanumber/openWARData", build_vignettes = TRUE)
-devtools::install_github("cpsievert/pitchRx", build_vignettes = TRUE)
+devtools::install_github(
+  "cpsievert/pitchRx", build_vignettes = TRUE, INSTALL_opts = "--no-test-load")
