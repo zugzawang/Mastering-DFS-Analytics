053d9037351c0299e4be78bf9217b18a4c39b34f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-09-03 01:26:04 -0700

    script to render book thrice

---
+++ render.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for i in gitbook bootstrap tufte
+do
+  ln -sf _output.yml.${i} _output.yml
+  ln -sf index.Rmd.${i} index.Rmd
+  R -e "bookdown::render_book(input = 'index.Rmd', output_format = 'all')"
+  rm -fr ${i}_book
+  cp -rp _book ${i}_book
+done
+
