ffc022e87a35e5207c6104d0fc9bb9c75adab437
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-09-24 00:26:55 -0700

    enable HTML help!!

--- Software/Docker/3run-interactive.cmd
+++ Software/Docker/3run-interactive.cmd
@@ -1,4 +1,4 @@
-docker run --interactive --tty --user=dfstools --name=dfstools --publish=7777:7777 docker.io/znmeb/mastering-dfs-analytics:latest /home/dfstools/Scripts/start-jupyter.bash
+docker run --interactive --tty --user=dfstools --name=dfstools --publish=7557:7557 --publish=7777:7777 docker.io/znmeb/mastering-dfs-analytics:latest /home/dfstools/Scripts/start-jupyter.bash
 docker commit dfstools docker.io/znmeb/mastering-dfs-analytics:latest
 docker rm dfstools
 docker images
---
+++ Software/Docker/start-help.R
@@ -0,0 +1,6 @@
+# Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
+# License: AGPL-3.0
+
+options(help.ports = 7557)
+tools::startDynamicHelp()
+print("Browse to http://127.0.0.1:7557/doc/html/index.html for help")
