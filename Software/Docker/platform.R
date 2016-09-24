212d3547c270dc86081bce7b9983c9381849ec8d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-09-24 00:13:34 -0700

    cleanup

--- Software/Docker/platform.R
+++ Software/Docker/platform.R
@@ -2,25 +2,13 @@
 # License: AGPL-3.0
 
 install.packages(c(
-  "brew",
-  "broom",
-  "crayon",
   "devtools",
-  "dplyr",
-  "evaluate",
-  "forcats",
-  "ggplot2",
   "googlesheets",
   "pbdZMQ",
-  "praise",
-  "readr",
-  "readxl",
   "repr",
   "roxygen2",
   "RSQLite",
-  "testthat",
-  "tidyr",
-  "uuid"),
+  "tidyverse"),
   quiet = TRUE)
 devtools::install_github("IRkernel/IRdisplay", build_vignettes = TRUE)
 devtools::install_github("IRkernel/IRkernel", build_vignettes = TRUE)
