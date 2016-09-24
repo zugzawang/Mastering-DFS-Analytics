8237070a362555502a2b29f2e1e567c9128caedf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-09-24 01:17:38 -0700

    Revert "enable HTML help!!"
    This reverts commit ffc022e87a35e5207c6104d0fc9bb9c75adab437.

--- Software/Docker/3run-detached.cmd
+++ Software/Docker/3run-detached.cmd
@@ -1,1 +1,1 @@
-docker run --detach --user=dfstools --name=dfstools --publish=7557:7557 --publish=7777:7777 docker.io/znmeb/mastering-dfs-analytics:latest /home/dfstools/Scripts/start-jupyter.bash
+docker run --detach --user=dfstools --name=dfstools --publish=7777:7777 docker.io/znmeb/mastering-dfs-analytics:latest /home/dfstools/Scripts/start-jupyter.bash
