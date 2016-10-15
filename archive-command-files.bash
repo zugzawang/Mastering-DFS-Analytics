e36cfbb0593e8c878bcc6476627ced6730f4510e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-10-15 14:20:28 -0700

    command files now at the top

--- archive-command-files.bash
+++ archive-command-files.bash
@@ -3,5 +3,5 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: AGPL-3
 
-rm ../../docker-commands.zip
-zip -9r ../../docker-commands.zip *.cmd
+rm docker-commands.zip
+zip -9r docker-commands.zip *.cmd
