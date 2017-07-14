3419bef90bf7d3c6aa12d22d6b07eabf6e6dffa3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-07-13 21:53:41 -0700

    add missing import

--- dfstools/R/contest_history.R
+++ dfstools/R/contest_history.R
@@ -2,6 +2,7 @@
 #' @name read_draftkings_contest_history
 #' @description Read a DraftKings contest history CSV into a tibble
 #' @export read_draftkings_contest_history
+#' @import readr
 #' @param file CSV history file from DraftKings
 #' @return a tibble of the contest history
 #' @examples
@@ -11,7 +12,7 @@
 #' }
 
 read_draftkings_contest_history <- function(file) {
-	df <- readr::read_csv(file, col_types = cols(
+	df <- read_csv(file, col_types = cols(
     Contest_Date_EST = col_character(),
     Entry_Fee = col_number(),
     Prize_Pool = col_number(),
